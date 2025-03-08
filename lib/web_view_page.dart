59d512164e4a02843d1d94806c8f290adaf175c5
Cisco Ramon <aungkoman@gmail.com>
2025-03-08 19:04:03 +0630

    စိတ်ဓာတ်ကျစရာ flutter ကမ္ဘာ

--- lib/web_view_page.dart
+++ lib/web_view_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class WebViewPage extends StatefulWidget {
   final String url;
@@ -13,9 +14,52 @@ class WebViewPage extends StatefulWidget {
 }
 
 class _WebViewPageState extends State<WebViewPage> {
+
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google WebView'),
+      ),
+      body: InAppWebView(
+        key: webViewKey,
+        initialUrlRequest: URLRequest(url: WebUri('https://www.google.com')),
+        onWebViewCreated: (controller) {
+          webViewController = controller;
+        },
+        androidOnPermissionRequest: (controller, origin, resources) async {
+          return PermissionRequestResponse(
+              resources: resources,
+              action: PermissionRequestResponseAction.GRANT);
+        },
+        shouldOverrideUrlLoading: (controller, navigationAction) async {
+          var uri = navigationAction.request.url!;
+
+          if (!["http", "https", "file", "chrome", "data", "javascript", "about"]
+              .contains(uri.scheme)) {
+            // Handle external URLs here (e.g., open in the device's browser)
+            // You might want to use url_launcher for this:
+            // if (await canLaunchUrl(uri)) {
+            //   await launchUrl(uri);
+            //   return NavigationActionPolicy.CANCEL;
+            // }
+
+            return NavigationActionPolicy.ALLOW; //or CANCEL depending on your need.
+          }
+          return NavigationActionPolicy.ALLOW;
+        },
+        onLoadStop: (controller, url) async {
+
+        },
+        onProgressChanged: (controller, progress) {
+
+        },
+      ),
+    );
   }
   
 }
