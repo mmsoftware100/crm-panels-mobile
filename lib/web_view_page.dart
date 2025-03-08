9df0952fc58d9b7e5b27e3907fe434b4556b6e06
Cisco Ramon <aungkoman@gmail.com>
2025-03-08 19:17:48 +0630

    basic work flow ok

--- lib/web_view_page.dart
+++ lib/web_view_page.dart
@@ -16,6 +16,7 @@ class WebViewPage extends StatefulWidget {
 class _WebViewPageState extends State<WebViewPage> {
 
   final GlobalKey webViewKey = GlobalKey();
+  int progress = 0;
 
   InAppWebViewController? webViewController;
 
@@ -25,40 +26,97 @@ class _WebViewPageState extends State<WebViewPage> {
       appBar: AppBar(
         title: Text('Google WebView'),
       ),
-      body: InAppWebView(
-        key: webViewKey,
-        initialUrlRequest: URLRequest(url: WebUri('https://www.google.com')),
-        onWebViewCreated: (controller) {
-          webViewController = controller;
-        },
-        androidOnPermissionRequest: (controller, origin, resources) async {
-          return PermissionRequestResponse(
-              resources: resources,
-              action: PermissionRequestResponseAction.GRANT);
-        },
-        shouldOverrideUrlLoading: (controller, navigationAction) async {
-          var uri = navigationAction.request.url!;
-
-          if (!["http", "https", "file", "chrome", "data", "javascript", "about"]
-              .contains(uri.scheme)) {
-            // Handle external URLs here (e.g., open in the device's browser)
-            // You might want to use url_launcher for this:
-            // if (await canLaunchUrl(uri)) {
-            //   await launchUrl(uri);
-            //   return NavigationActionPolicy.CANCEL;
-            // }
-
-            return NavigationActionPolicy.ALLOW; //or CANCEL depending on your need.
+      body: WillPopScope(
+        onWillPop: ()async{
+          print("onWillPop");
+          print(webViewController?.canGoBack());
+          bool stauts = await webViewController?.canGoBack() ?? false;
+          print(stauts);
+          if(stauts){
+            webViewController?.goBack();
+            return false;
           }
-          return NavigationActionPolicy.ALLOW;
+          else{
+            return true;
+          }
         },
-        onLoadStop: (controller, url) async {
+        child: Column(
+          children: [
+            Text(progress.toString()),
+            LinearProgressIndicator(
+              value: progress / 100,
+              color: Colors.red,
+            ),
+            Expanded(
+              child: InAppWebView(
+                key: webViewKey,
+                // initialUrlRequest: URLRequest(url: WebUri('https://www.google.com')),
+                initialUrlRequest: URLRequest(url: WebUri('https://crm.software100.com.mm/panels')),
+                onWebViewCreated: (controller) {
+                  webViewController = controller;
+                },
+                shouldOverrideUrlLoading: (controller, navigationAction) async {
+                  var uri = navigationAction.request.url!;
+              
+                  if (!["http", "https", "file", "chrome", "data", "javascript", "about"]
+                      .contains(uri.scheme)) {
+                    // Handle external URLs here (e.g., open in the device's browser)
+                    // You might want to use url_launcher for this:
+                    // if (await canLaunchUrl(uri)) {
+                    //   await launchUrl(uri);
+                    //   return NavigationActionPolicy.CANCEL;
+                    // }
+              
+                    return NavigationActionPolicy.ALLOW; //or CANCEL depending on your need.
+                  }
+                  return NavigationActionPolicy.ALLOW;
+                },
+                onLoadStop: (controller, url) async {
+              
+                },
+                onProgressChanged: (controller, webProgress) {
+                  print("onProgressChanged");
+                  print(progress);
+                  setState(() {
+                    progress = webProgress;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
 
-        },
-        onProgressChanged: (controller, progress) {
-
-        },
       ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () async {
+                  if (await webViewController?.canGoBack() ?? true) {
+                    webViewController?.goBack();
+                  }
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.arrow_forward),
+                onPressed: () async {
+                  if (await webViewController?.canGoForward() ?? false) {
+                    webViewController?.goForward();
+                  }
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.refresh),
+                onPressed: () {
+                  webViewController?.reload();
+                },
+              ),
+            ],
+          ),
+        )
+
     );
   }
   
